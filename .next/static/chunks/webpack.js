c0409f4ea05fe0fa9d161e9715e2eafd7b73e471
meisprout <eahxmaala@gmail.com>
2023-02-25 17:32:20 +0800

    updated commission page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bdf3d26b6c7e195e"; }
+/******/ 		__webpack_require__.h = function() { return "9e5f7b1d36403599"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/commissions.js
+++ pages/commissions.js
@@ -1,19 +1,24 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import React from 'react';
-import { getAllContact, getRates } from "../lib/notion"
+import { getAddtlFees, getAllContact, getPayInfo, getRates } from "../lib/notion"
 import styles from '../styles/Commissions.module.css'
 import Footer from '../components/footer'
 import Navbar from '../components/navbar'
+import ReactMarkdown from 'react-markdown'
 
 export const getStaticProps = async () => {
   const rates = await getRates()
   const contact = await getAllContact()
+  const addfees = await getAddtlFees()
+  const payinfo = await getPayInfo()
 
   return {
     props: {
       rates: rates,
-      contact: contact
+      contact: contact,
+      addfees: addfees,
+      payinfo:payinfo
     },
     revalidate: 60
   };
@@ -29,24 +34,48 @@ export default function Commissions({...props}) {
       </Head>
       <Navbar/>
       <main className={styles.main}>
-      <h1>Commission Info</h1>
-      <section className={styles.ratesection}>
-        {
-          props.rates.map((rts, index)=>(
-            <div key={index} className={styles.rates}>
-            <div className={styles.container}>
-            <Image loader={() => rts.sample} src={rts.sample} layout="fill" objectFit="cover" loading='lazy'/>
+        <h1>Commission Info</h1>
+        <section>
+          <h2>Rates</h2>
+          <div className={styles.ratecontainer}>
+            {
+              props.rates.map((rts, index)=>(
+                <div key={index} className={styles.rates}>
+                  <div className={styles.container}>
+                    <Image loader={() => rts.sample} src={rts.sample} layout="fill" objectFit="cover" loading='lazy'/>
+                  </div>
+
+                  <div className={styles.ratestext}>
+                    <h2>{rts.name}</h2>
+                    <p>{rts.pesos} PHP</p>
+                  </div>
+                </div>
+              ))
+            }
             </div>
-              <div className={styles.ratestext}>
-                <h2>{rts.name}</h2>
-                <p>{rts.pesos} PHP</p>
-              </div>
-              
-            </div>
-          ))
-        }
-      </section>
-      
+        </section>
+
+        <section className={styles.addfees}>
+          <h2>Additional Fees</h2>
+          <div className={styles.addfeescontainer}>
+          {
+              props.addfees.map((add, index)=>(
+                <div key={index} className={styles.addfeesinfo}>
+                <h3>{add.name}</h3>
+                      <span className={styles.addfeeprice}>{add.pesos}</span>
+                      <p>{add.notes}</p>
+                </div>
+              ))
+            }
+          </div>
+            
+        </section>
+
+        <section className={styles.payinfo}>
+          <h2>Payment Information</h2>
+          
+          <ReactMarkdown>{props.payinfo.markdown}</ReactMarkdown>
+        </section>
       
       </main>
       <Footer data={props.contact}/>
